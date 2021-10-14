df9de78b4828c0a54bdaea0b48333b13a468567b
kcho32 <kwdc0302@gmail.com>
2021-10-14 17:22:03 +0900

    FEAT: router added

--- src/App.js
+++ src/App.js
@@ -3,34 +3,52 @@ import GroupContainer from './Authentication/Containers/GroupContainer';
 import LoginContainer from './Authentication/Containers/LoginContainer';
 import RegisterContainer from './Authentication/Containers/RegisterContainer';
 import BoardInputContainer from './Board/Containers/BoardInputContainer';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-        </header>
-        <body>
-          <RegisterContainer/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <LoginContainer/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <GroupContainer/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <BoardInputContainer />
-        </body>
-        <footer></footer>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to = '/'/>
+              </li>
+              <li>
+                <Link to = '/Home/'/>
+              </li>
+              <li>
+                <Link to = '/authentication/'/>
+              </li>
+              <li>
+                <Link to = '/schedule/'/>
+              </li>
+              <li>
+                <Link to = '/MyPage/'/>
+              </li>
+              <li>
+                <Link to = '/Board/'/>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/home/">
+              {/* <Home /> */}
+            </Route>
+            <Route path="/home/">
+              {/* <Home /> */}
+            </Route>
+            <Route path="/home/">
+              {/* <Home /> */}
+            </Route>
+            <Route path="/home/">
+              {/* <Home /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
