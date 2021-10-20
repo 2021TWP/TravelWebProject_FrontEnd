f54de0f635197bccba8188fc396bb2666b2c70ac
kcho32 <kwdc0302@gmail.com>
2021-10-20 16:58:18 +0900

    FEAT: Reset PassWord

--- src/App.js
+++ src/App.js
@@ -4,51 +4,68 @@ import LoginContainer from './Authentication/Containers/LoginContainer';
 import RegisterContainer from './Authentication/Containers/RegisterContainer';
 import BoardInputContainer from './Board/Containers/BoardInputContainer';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from './Header'
+import EmailConfirmedContainer from './Authentication/Containers/EmailConfirmedContainer';
+import PasswordResetContainer from './Authentication/Containers/PasswordResetContainer';
 
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to = '/'/>
-              </li>
-              <li>
-                <Link to = '/Home/'/>
-              </li>
-              <li>
-                <Link to = '/authentication/'/>
-              </li>
-              <li>
-                <Link to = '/schedule/'/>
-              </li>
-              <li>
-                <Link to = '/MyPage/'/>
-              </li>
-              <li>
-                <Link to = '/Board/'/>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/home/">
-              {/* <Home /> */}
-            </Route>
-            <Route path="/home/">
-              {/* <Home /> */}
-            </Route>
-            <Route path="/home/">
-              {/* <Home /> */}
-            </Route>
-            <Route path="/home/">
-              {/* <Home /> */}
-            </Route>
-          </Switch>
-        </div>
+        {window.location.href !== 'http://localhost:3000/authentication/login' && window.location.href !=="http://localhost:3000/authentication/signup"
+        && window.location.href !== 'http://localhost:3000/authentication/login/' && window.location.href !=="http://localhost:3000/authentication/signup/"
+        ? <Header/> 
+        : null}
+        <Switch>
+          <Route exact path="/authentication/login/" component={LoginContainer}/>
+          <Route exact path="/authentication/signup/" component={RegisterContainer}/>
+          <Route exact path="/authentication/emailconfirmed/" component={EmailConfirmedContainer}/>
+          <Route exact path="/authentication/password/reset/" component={PasswordResetContainer} />
+
+
+        </Switch>
       </Router>
+      // <Router>
+      //   <div>
+      //     <nav>
+      //       <ul>
+      //         <li>
+      //           <Link to = '/'/>
+      //         </li>
+      //         <li>
+      //           <Link to = '/Home/'/>
+      //         </li>
+      //         <li>
+      //           <Link to = '/authentication/'/>
+      //         </li>
+      //         <li>
+      //           <Link to = '/schedule/'/>
+      //         </li>
+      //         <li>
+      //           <Link to = '/MyPage/'/>
+      //         </li>
+      //         <li>
+      //           <Link to = '/Board/'/>
+      //         </li>
+      //       </ul>
+      //     </nav>
+      //     <Switch>
+      //       <Route path="/home/">
+      //         {/* <Home /> */}
+      //       </Route>
+      //       <Route path="/home/">
+      //         {/* <Home /> */}
+      //       </Route>
+      //       <Route path="/home/">
+      //         {/* <Home /> */}
+      //       </Route>
+      //       <Route path="/home/">
+      //         {/* <Home /> */}
+      //       </Route>
+      //     </Switch>
+      //   </div>
+      // </Router>
     )
   }
 }
