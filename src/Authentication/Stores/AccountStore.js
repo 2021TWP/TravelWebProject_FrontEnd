374b1b2b8dc9cd469e56f5810f05e2d887a9263b
kcho32 <kwdc0302@gmail.com>
2021-10-27 11:15:52 +0900

    FIX: password reset error message fixed

--- src/Authentication/Stores/AccountStore.js
+++ src/Authentication/Stores/AccountStore.js
@@ -154,10 +154,10 @@ class AccountStore {
             const data = await accountApi.changePassword(this.user)
             if ('detail' in data) {
                 alert(data.detail);
+                window.location.href='javascript:history.back()'
             }else{
                 runInAction(() => this.error_message = {...this.error_message, ...data})
             }
-            window.location.href='/'
         }catch(error) {
             runInAction(() => this.message = error.message)
         }
--- src/Authentication/Views/PasswordResetConfirmView.js
+++ src/Authentication/Views/PasswordResetConfirmView.js
@@ -58,7 +58,7 @@ export default class LoginView extends Component {
                 id="password1"
                 value={user.password1} 
                 onChange={(e)=>setProps(e.target.name, e.target.value)}
-                onClick={(e)=>onClickEvent(e.target.name)}
+                onClick={(e)=>onClickEvent("new_password1")}
               />
                 {(message.new_password1 !== "")
                 ? message.new_password1
@@ -73,7 +73,7 @@ export default class LoginView extends Component {
                 id="password2"
                 value={user.password2} 
                 onChange={(e)=>setProps(e.target.name, e.target.value)}
-                onClick={(e)=>onClickEvent(e.target.name)}
+                onClick={(e)=>onClickEvent("new_password2")}
               />
                 {(message.new_password2 !== "")
                 ? message.new_password2
@@ -92,18 +92,5 @@ export default class LoginView extends Component {
         </Container>
       </ThemeProvider>
     );    
-            
-            
-            
-            // <div>
-            //     <input type="text" name="username" placeholder="아이디" value={user.username} onChange={(e)=>setProps(e.target.name, e.target.value)}/>
-            //     {message.username}<br/>
-            //     <input type="password" name="password1" placeholder="비밀번호" value={user.password1} onChange={(e)=>setProps(e.target.name, e.target.value)}/>
-            //     {message.password}<br/>
-            //     <button onClick={()=>handleLoginSubmit()}>로그인</button>
-            //     <button>회원 가입</button>
-            //     <a onClick={()=>handleLogoutSubmit()}>로그아웃</a>
-            //     <button onClick={()=>{test()}}>테스트</button>
-            // </div>
-    }
+  }
 }
