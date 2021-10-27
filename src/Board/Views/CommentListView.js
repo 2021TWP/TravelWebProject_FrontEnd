dc1129f088597c687f1979a85787cc4f9af81ea0
hanbikimm <zacquisha130@gmail.com>
2021-10-27 11:28:36 +0900

    FEAT: comment_modify

--- src/Board/Views/CommentListView.js
+++ src/Board/Views/CommentListView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import {BsPen} from 'react-icons/bs'
 import {GrTrash} from 'react-icons/gr'
+import Stack from '@mui/material/Stack';
 
 class CommentListView extends Component {
     render() {
@@ -9,14 +10,14 @@ class CommentListView extends Component {
         let comment_date = this.props.comment.date;
         return (
             <div onClick={()=>onSelect(comment)}>
+                <Stack m={2} direction="row">
                 {comment.comment_content} &nbsp; &nbsp;
                 {comment.user_id} &nbsp; &nbsp;
                 {moment(comment_date).format(('YYYY. MM. DD.'))} &nbsp; &nbsp;
                 {sessionStorage.getItem('id') == comment.user_id ?
                 <span><BsPen onClick={()=>commentModify()}/> &nbsp;
-                <GrTrash onClick={()=>commentRemove()}/></span> : <div></div>
-
-                }
+                <GrTrash onClick={()=>commentRemove(comment.id)}/></span> : <div></div>}
+                </Stack>
 
             </div>
         );
